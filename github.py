eff4c7b95addeeca371ff9bd2e253e1683464c49
nitishtlgt <nitishtlgt@gmail.com>
2023-11-28 13:32:27 +0530

    webdriver chrome install manager

--- github.py
+++ github.py
@@ -3,20 +3,15 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-
 
 @pytest.fixture
 def browser():
-    # options = Options()
-    # chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument("--headless")
-    # chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--disable-dev-shm-usage')
-    # driver = webdriver.Chrome(options=chrome_options)
-    driver_path = ChromeDriverManager().install()
-    driver = webdriver.Chrome()
-    # driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+    options = Options()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(options=chrome_options)
     yield driver
     driver.quit()
 
